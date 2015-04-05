340ba96dbc2369aa3309d9017e452a9e3d83eb5e
Jason Frame <jason@onehackoranother.com>
2015-04-05 15:25:38 +0100

    working

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-module.exports = function() {
-	
+var BASE = {
+	'C'		: 24,
+	'D'		: 26,
+	'E'		: 28,
+	'F'		: 29,
+	'G'		: 31,
+	'A'		: 33,
+	'B'		: 35
+};
+
+var KEYS = {};
+(function() {
+	for (var octave = -2; octave <= 8; ++octave) {
+		for (var k in BASE) {
+			var key = BASE[k] + (octave * 12);
+			KEYS[k + 'b' + octave] = key - 1;
+			KEYS[k + octave] = key;
+			KEYS[k + '#' + octave] = key + 1;
+		}
+	}
+})();
+
+exports.key = key;
+function key(note) {
+	return KEYS[note];
 }
