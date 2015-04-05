05910afee9226b62afa49aeba43244f41ec65990
Jason Frame <jason@onehackoranother.com>
2015-04-05 15:36:14 +0100

    tabs => spaces

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 var BASE = {
-	'C'		: 24,
-	'D'		: 26,
-	'E'		: 28,
-	'F'		: 29,
-	'G'		: 31,
-	'A'		: 33,
-	'B'		: 35
+    'C'     : 24,
+    'D'     : 26,
+    'E'     : 28,
+    'F'     : 29,
+    'G'     : 31,
+    'A'     : 33,
+    'B'     : 35
 };
 
 var KEYS = {};
 (function() {
-	for (var octave = -2; octave <= 8; ++octave) {
-		for (var k in BASE) {
-			var key = BASE[k] + (octave * 12);
-			KEYS[k + 'b' + octave] = key - 1;
-			KEYS[k + octave] = key;
-			KEYS[k + '#' + octave] = key + 1;
-		}
-	}
+    for (var octave = -2; octave <= 8; ++octave) {
+        for (var k in BASE) {
+            var key = BASE[k] + (octave * 12);
+            KEYS[k + 'b' + octave] = key - 1;
+            KEYS[k + octave] = key;
+            KEYS[k + '#' + octave] = key + 1;
+        }
+    }
 })();
 
 module.exports = function(note) {
-	return KEYS[note];
+    return KEYS[note];
 }
--- test/all.js
+++ test/all.js
@@ -3,13 +3,13 @@ var key = require('../');
 
 test('some keys', function(assert) {
    
-	assert.equal(key('C-2'), 0);
-	assert.equal(key('C0'), 24);
+    assert.equal(key('C-2'), 0);
+    assert.equal(key('C0'), 24);
 
-	assert.equal(key('Cb1'), 35);
-	assert.equal(key('C1'), 36);
-	assert.equal(key('C#1'), 37);
-	
-	assert.end();
+    assert.equal(key('Cb1'), 35);
+    assert.equal(key('C1'), 36);
+    assert.equal(key('C#1'), 37);
+    
+    assert.end();
 
 });
