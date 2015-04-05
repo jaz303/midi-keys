1339f84cdceefce4ea276dd377c56157a614451d
Jason Frame <jason@onehackoranother.com>
2015-04-05 15:29:05 +0100

    some tests

--- test/all.js
+++ test/all.js
@@ -1,6 +1,15 @@
 var test = require('tape');
+var key = require('../');
 
-test('truth', function(assert) {
-    assert.ok(true);
-    assert.end();
+test('some keys', function(assert) {
+   
+	assert.equal(key('C-2'), 0);
+	assert.equal(key('C0'), 24);
+
+	assert.equal(key('Cb1'), 35);
+	assert.equal(key('C1'), 36);
+	assert.equal(key('C#1'), 37);
+	
+	assert.end();
+
 });
